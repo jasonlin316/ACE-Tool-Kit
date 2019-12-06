6ccb6a4981ddaa547e2e1b40d0d74cf092771524
Angel747 <wh.taiwan56@gmail.com>
2019-12-06 11:07:58 +0800

    pin unpin

--- Innovation Program1.py
+++ Innovation Program1.py
@@ -1,21 +1,44 @@
 import os
 
-taskbar=['calc','notepad']
-cmd='cmd /k "\
-    syspin "C:\Windows\\System32\\'+taskbar[0]+'.exe\" 5386 &&  \
-    syspin "C:\Windows\\'+taskbar[1]+'.exe\" 5386 \
-    "'
+taskbar=['calc','mspaint']
+
+#os.system('cmd /k "syspin "C:\Windows\\System32\\calc.exe\" 5386 && "')   
+              
+cmd='cmd /k "'
+def pin(): #pin
+    global cmd
+    for i in range(len(taskbar)):
+        cmd=cmd+'syspin "C:\Windows\\System32\\'+taskbar[i]+'.exe\" 5386 '
+        if i!=len(taskbar)-1:
+            cmd=cmd+'&& '
+        else:
+            cmd=cmd+'"'
+        
+def unpin(): #unpin
+    global cmd
+    for i in range(len(taskbar)):
+        cmd=cmd+'syspin "C:\Windows\\System32\\'+taskbar[i]+'.exe\" 5387 '
+        if i!=len(taskbar)-1:
+            cmd=cmd+'&& '
+        else:
+            cmd=cmd+'"'
+        
+#    syspin "C:\Windows\\System32\\'+taskbar[0]+'.exe\" 5386 &&  \
+#    syspin "C:\Windows\\'+taskbar[1]+'.exe\" 5386 \
+#    "'
+pin()
+print(cmd)
 os.system(cmd)
-
-#5386:pin to taskbar
-#5387:unpin from taskbar
-
-#school:word, excel, powerpoint
-#office:word, excel, powerpoint, outlook, skype
-#home:spotify, netflix
-
-a=1
-
+#
+##5386:pin to taskbar
+##5387:unpin from taskbar
+#
+##school:word, excel, powerpoint
+##office:word, excel, powerpoint, outlook, skype
+##home:spotify, netflix
+#
+#a=1
+#
 #if a==1: #school-->office
 #    os.system('cmd /k "\
 #              syspin "C:\Windows\\System32\\outlook.exe\" 5386  && \
@@ -67,3 +90,4 @@ a=1
 #              syspin "C:\Windows\\System32\\outlook.exe\" 5386  && \
 #              syspin "C:\Windows\\System32\\skype.exe\" 5386 \
 #              "')
+    
