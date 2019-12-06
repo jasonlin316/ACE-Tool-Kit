707c740cbc71fca663717be68a69dde4e882e984
Yi Chien Lin <jasonlin316@gmail.com>
2019-12-06 11:38:43 +0800

    version 1 complete

--- SSD_v1.py
+++ SSD_v1.py
@@ -1,15 +1,6 @@
 import os
-import time
-#5386:pin to taskbar
-#5387:unpin from taskbar
 
-#school:word, excel, powerpoint
-#office:word, excel, powerpoint, outlook, skype
-#home:spotify, netflix
-
-a=1
-new_mode = 0
-curr_mode = 0
+taskbar=['calc','mspaint']
 
 apps = []
 
@@ -28,54 +19,32 @@ apps.append(school)
 
 home.append("WINWORD.EXE")
 apps.append(home)
-cmd='cmd /c "'
 
+new_mode = 0
+curr_mode = 0
+           
+cmd='cmd /c "'
 def pin(new_mode): #pin
     global cmd
-    global apps
     for i in range(len(apps[new_mode])):
         cmd=cmd+'syspin "C:\Windows\\System32\\'+apps[new_mode][i]+'" 5386 '
         if i!=len(apps[new_mode])-1:
             cmd=cmd+'&& '
         else:
             cmd=cmd+'"'
-        
+    os.system(cmd)
+    cmd = '' 
+
 def unpin(curr_mode): #unpin
     global cmd
-    global apps
     for i in range(len(apps[curr_mode])):
         cmd=cmd+'syspin "C:\Windows\\System32\\'+apps[curr_mode][i]+'" 5387 '
         if i!=len(apps[curr_mode])-1:
             cmd=cmd+'&& '
         else:
             cmd=cmd+'"'
-
-def mode_change(curr_mode,new_mode):
-    curr_str = ''
-    new_str = ''
-    cmd = ''
-    
-    unpin(curr_mode)
-    pin(new_mode)
-    '''
-    for i in range(len(apps[curr_mode])):
-        curr_str += 'syspin "'
-        curr_str += apps[int(curr_mode)][i]
-        curr_str += '" 5837 && '
-    
-    for i in range(len(apps[int(new_mode)])):
-        new_str += 'syspin "'
-        new_str += apps[int(new_mode)][i]
-        if i == len(apps[int(new_mode)])-1:
-            new_str += '" 5836" '
-        else:
-            new_str += '" 5836 && '
-
-    cmd = ' cmd /c "' + curr_str + new_str
-    '''
-    print(cmd)
     os.system(cmd)
-
+    cmd = '' 
 
 while True:
     new_mode = input('enter mode:')
@@ -86,6 +55,11 @@ while True:
         break
 
     if new_mode != curr_mode:
-        mode_change(curr_mode,new_mode)
+        unpin(curr_mode)
+        pin(new_mode)
 
     curr_mode = new_mode
+#
+##5386:pin to taskbar
+##5387:unpin from taskbar
+#
